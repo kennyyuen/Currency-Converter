b8562128f5b8b87f830396558462be0db7cffe11
kennyyuen <kennyyuen2008@gmail.com>
2018-10-11 16:47:42 +0800

    new UI

--- script.js
+++ script.js
@@ -6,14 +6,22 @@ var targetcountrylist = ["USD","HKD"];
 var commonlist = ["USD","HKD","JPY","EUR","CAD"];
 var histrates = [];
 
+//rates class
 class Rates {
     constructor(country, rate){
         this.rate = rate;
         this.country = country;
+        if(country != "BTC"){
+            this.code = country.substring(0,2);
+        }
+        else {
+            this.code = country;
+        }
     }
     
 }
 
+//Get rate function
 function getRateByCountry(country){
     for (var i = 0;i < totalcountries;i++){
         if (rates[i].country == country){
@@ -21,11 +29,53 @@ function getRateByCountry(country){
         }
     }
 }
-
+//Get historical rate
 function getHistRateByCountry(country){
     for (var i = 0;i < histtotalcountries;i++){
         if (histrates[i].country == country){
             return histrates[i].rate;
+        }
+    }
+}
+
+//Dropdown function
+function setDropDownList(id,parentID){
+    var div = document.getElementById(id);
+    for(var i = 0;i < totalcountries;i++){
+        var temp = document.createElement("a");
+        var image = "<img class='flag' alt='' src='https://www.countryflags.io/"+ rates[i].code + "/flat/32.png'>";
+        if(rates[i].code == "BTC"){
+            image = "<img src='images/BTC.png'>";
+        }
+        temp.id = i;
+        temp.innerHTML = image + rates[i].country;
+        temp.onclick = listClicked;
+        temp.setAttribute("parentButtonID",parentID);  
+        div.appendChild(temp);
+    }
+}
+// dropdown button
+function dropDownClicked(id) {
+    document.getElementById(id).classList.toggle("show");
+}
+// dropdown list clicked
+function listClicked(){
+    console.log(this.id);
+    document.getElementById(this.getAttribute("parentButtonID")).innerHTML = document.getElementById(this.id).innerHTML;
+    document.getElementById(this.parentNode.id).classList.toggle("show");
+}
+// dropdown search function
+function filterFunction(dropdowndivID,inputID) {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById(inputID);
+    filter = input.value.toUpperCase();
+    div = document.getElementById(dropdowndivID);
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
         }
     }
 }
@@ -58,6 +108,7 @@ function setCountryList(id,select){
     }
 }
 
+//calucate function
 function calRate(base,target){
     var brate = getRateByCountry(base);
     var trate = getRateByCountry(target);
@@ -66,6 +117,7 @@ function calRate(base,target){
     return r.toFixed(2);
 }
 
+//table function
 function addBase(base){
     var flag = false;
     for (var i = 0;i < basecountrylist.length;i++){
@@ -139,6 +191,105 @@ function setTable(){
     }
 }
 
+//show historical rate
+function showRate(){
+    var bCountry = document.getElementById("histbase").options[document.getElementById("histbase").selectedIndex].text;
+    var tCountry = document.getElementById("histtarget").options[document.getElementById("histtarget").selectedIndex].text;
+    var brate = getHistRateByCountry(bCountry);
+    console.log(brate);
+    var trate = getHistRateByCountry(tCountry);
+    var r = (1 * brate / trate).toFixed(2);
+    var baseicon = "<img height='30' width='30' alt='' src='images/" + bCountry + ".png'/>";
+    var taricon = "<img height='30' width='30' alt='' src='images/" + tCountry + ".png'/>";
+    document.getElementById("histresult").innerHTML = baseicon + "<span id='histbaseindicate'>  " + bCountry + "1  = </span>" + taricon + " " + tCountry + " " + r;
+    
+}
+
+function showResult(){
+    /*
+    var bCountryid = $('#base').val();
+    var tCountryid = $('#target').val();
+    var brate = rates[bCountryid].rate;
+    var trate = rates[tCountryid].rate;
+    var a = $('#amount').val();
+    var r = (a * brate / trate).toFixed(2);
+    var baseicon = "<img height='30' width='30' alt='' src='images/" + rates[bCountryid].country + ".png'/>";
+    var taricon = "<img height='30' width='30' alt='' src='images/" + rates[tCountryid].country + ".png'/>";
+    document.getElementById("result").innerHTML = baseicon + "<span id='baseindicate'>  " + rates[bCountryid].country + " " + a + " = </span>" + taricon + " " + rates[tCountryid].country + " " + r;
+    */
+    
+    
+}
+
+
+//button event function
+//nav bar button
+function tabClicked(tab){
+    if(tab == "home"){
+        document.getElementById("home").style.display = 'block';
+        $('#barhome').toggleClass('act');
+        $('#barhis').toggleClass('act');
+        document.getElementById("hist").style.display = 'none';
+    }
+    else if(tab == "hist"){
+        document.getElementById("home").style.display = 'none';
+        $('#barhome').toggleClass('act');
+        $('#barhis').toggleClass('act');
+        document.getElementById("hist").style.display = 'block';
+        init("hist");
+    }
+        
+}
+
+//orientation button
+function orienClicked(){
+    var image = document.getElementById("indicateimage");
+    if(image.innerHTML == "🡣") {
+        image.innerHTML = "🡢";
+    }
+    else {
+        image.innerHTML = "🡣";
+    }
+}
+
+function tablechange(id,n){
+    if(id == "base"){
+        basecountrylist[n] = rates[$('#tablebase'+ n).val()].country;
+        setTable();
+    }
+    else {
+        targetcountrylist[n] = rates[$('#tabletarget'+ n).val()].country;
+        setTable();
+    }
+}
+
+//initialize function
+function init(temp){
+    loadJSON(function(response){
+        var p = JSON.parse(response);
+        var date = $('#date-input').val();
+        if(temp == "latest"){
+            getData(p.key,"latest");
+        }
+        else {
+            getData(p.key,date);
+        }
+    });
+}
+
+//Read key from json file
+function loadJSON(callback){
+    var handler = new XMLHttpRequest();
+    handler.overrideMimeType("application/json");
+    handler.open('GET', 'key.json', true);
+    handler.onreadystatechange = function () {
+        if (handler.readyState == 4 && handler.status == "200") {
+            callback(handler.responseText);
+        }
+    };
+    handler.send(null);
+}
+
 function getData(key,date){
     var url = "http://data.fixer.io/api/" + date + "?access_key=" + key + "&format=1";
     $.ajax({
@@ -156,13 +307,9 @@ function getData(key,date){
                 var c = Object.keys(value.rates)[i];
                 var r = value.rates[c];
                 rates[i] = new Rates(c,r);
-            }
-            setCountryList("base","HKD");
-            setCountryList("target","USD");
-            setCountryList("tablebase0","USD");
-            setCountryList("tabletarget0","USD");
-            setCountryList("tablebase1","HKD");
-            setCountryList("tabletarget1","HKD");
+            }            
+            //setDropDownList("bDrop","bButton");
+            //setDropDownList("tDrop","tButton");
             showResult();
         }
         else{
@@ -182,49 +329,22 @@ function getData(key,date){
     });
 }
 
-function showRate(){
-    var bCountry = document.getElementById("histbase").options[document.getElementById("histbase").selectedIndex].text;
-    var tCountry = document.getElementById("histtarget").options[document.getElementById("histtarget").selectedIndex].text;
-    var brate = getHistRateByCountry(bCountry);
-    console.log(brate);
-    var trate = getHistRateByCountry(tCountry);
-    var r = (1 * brate / trate).toFixed(2);
-    var baseicon = "<img height='30' width='30' alt='' src='images/" + bCountry + ".png'/>";
-    var taricon = "<img height='30' width='30' alt='' src='images/" + tCountry + ".png'/>";
-    document.getElementById("histresult").innerHTML = baseicon + "<span id='histbaseindicate'>  " + bCountry + "1  = </span>" + taricon + " " + tCountry + " " + r;
-    
-}
 
-function showResult(){
-    var bCountryid = $('#base').val();
-    var tCountryid = $('#target').val();
-    var brate = rates[bCountryid].rate;
-    var trate = rates[tCountryid].rate;
-    var a = $('#amount').val();
-    var r = (a * brate / trate).toFixed(2);
-    var baseicon = "<img height='30' width='30' alt='' src='images/" + rates[bCountryid].country + ".png'/>";
-    var taricon = "<img height='30' width='30' alt='' src='images/" + rates[tCountryid].country + ".png'/>";
-    document.getElementById("result").innerHTML = baseicon + "<span id='baseindicate'>  " + rates[bCountryid].country + " " + a + " = </span>" + taricon + " " + rates[tCountryid].country + " " + r;
-}
-
-function loadJSON(callback){
-    var handler = new XMLHttpRequest();
-    handler.overrideMimeType("application/json");
-    handler.open('GET', 'key.json', true);
-    handler.onreadystatechange = function () {
-        if (handler.readyState == 4 && handler.status == "200") {
-            callback(handler.responseText);
-        }
-    };
-    handler.send(null);
-}
-
+//changing theme
 function changeTheme(){
     var bt = document.getElementById("theme");
     if (bt.innerHTML == "Dark"){
         bt.innerHTML = "Light";
         bt.className = "btn btn-light";
-        document.getElementById("bar").className = "navbar navbar-expand-lg navbar-dark bg-dark";
+        
+        $('.trashcan').hide();
+        $('.trashcan-dark').show();
+        $('.countrybtn').toggleClass('btn-light');
+        $('.countrybtn').toggleClass('btn-dark');
+        
+        $('#bar').toggleClass('bg-dark');
+        $('#barhome').toggleClass('dark');
+        $('#barhis').toggleClass('dark');
         $('body').toggleClass('dark');
         $('#amount').toggleClass('dark');
         $('#base').toggleClass('dark');
@@ -240,7 +360,15 @@ function changeTheme(){
     else{
         bt.innerHTML = "Dark";
         bt.className = "btn btn-dark";
-        document.getElementById("bar").className = "navbar navbar-expand-lg navbar-light bg-light";
+        
+        $('.trashcan').show();
+        $('.trashcan-dark').hide();
+        $('.countrybtn').toggleClass('btn-light');
+        $('.countrybtn').toggleClass('btn-dark');
+        
+        $('#bar').toggleClass('bg-dark');
+        $('#barhome').toggleClass('dark');
+        $('#barhis').toggleClass('dark');
         $('body').toggleClass('dark');
         $('#amount').toggleClass('dark');
         $('#base').toggleClass('dark');
@@ -256,47 +384,6 @@ function changeTheme(){
     console.log("clicked");
 }
 
-function historyclicked(){
-    document.getElementById("home").style.display = 'none';
-    document.getElementById("barhome").className = "nav-item nav-link";
-    document.getElementById("barhis").className = "nav-item nav-link active";
-    document.getElementById("hist").style.display = 'block';
-    histinit();    
-}
-
-function homeclicked(){
-    document.getElementById("home").style.display = 'block';
-    document.getElementById("barhome").className = "nav-item nav-link active";
-    document.getElementById("barhis").className = "nav-item nav-link";
-    document.getElementById("hist").style.display = 'none';
-}
-
-function tablechange(id,n){
-    if(id == "base"){
-        basecountrylist[n] = rates[$('#tablebase'+ n).val()].country;
-        setTable();
-    }
-    else {
-        targetcountrylist[n] = rates[$('#tabletarget'+ n).val()].country;
-        setTable();
-    }
-}
-
-function histinit(){
-    loadJSON(function(response){
-        var p = JSON.parse(response);
-        var date = $('#date-input').val();
-        getData(p.key,date);
-    });
-}
-
-function init(){
-    loadJSON(function(response){
-        var p = JSON.parse(response);
-        getData(p.key,"latest");
-    });
-}
-
 $(document).ready(function(){
-    init();
+    init("latest");
 });
